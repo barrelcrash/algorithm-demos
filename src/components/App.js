733a0d1b0ec8b58b1f840f45603c74246e1994ad
Luke Abel <luke.abel@gmail.com>
2018-08-25 13:44:51 -0400

    chart working, but need to fix colors

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ValuesInput from './valuesInput';
+import SortChart from './SortChart';
 import {quicksort} from '../services/quicksort';
 import {createSortValue} from "../services/sortUtils";
 import '../styles/App.css';
@@ -15,39 +16,14 @@ function getArrayString(array) {
   return arrayString;
 }
 
-var BarChart = require("react-chartjs").Bar;
-
-var data = {
-  labels: ["", "", "", "", "", "", ""],
-	datasets: [
-		{
-			label: "My First dataset",
-			fillColor: ["rgba(220,220,220,0.5)", "rgba(220,0,220,0.5)"],
-			strokeColor: "rgba(220,220,220,0.8)",
-			data: [65, 70, 80, 81, 56, 55, 40]
-		}
-	]
-};
-
-var options = {
-  scaleBeginAtZero : true,
-	scaleShowGridLines : false,
-  barShowStroke : true, // stroke = outline
-	barStrokeWidth : 2,
-  barValueSpacing : 5, 
-  barDatasetSpacing : 1, // N/A
-  // {% raw %}
-	//String - A legend template
-	legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].fillColor%>\"><%if(datasets[i].label){%><%=datasets[i].label%><%}%></span></li><%}%></ul>"
-  // {% endraw %}
-};
 
 class App extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      algorithm: quicksort
+      algorithm: quicksort,
+      data: []
     }
 
     // seed default values for algorithm and input
@@ -58,10 +34,12 @@ class App extends Component {
     this.updateAndRun = this.updateAndRun.bind(this);
   }
 
+  // used by ValuesInput to update the values & graph
   updateAndRun(values) {
     this.state.algorithm.setValues(values);
     this.state.algorithm.run();
     this.setState({
+      data: this.state.algorithm.getStep(0).getValue(),
       firstStep: getArrayString(this.state.algorithm.getStep(0).getValue().map(x => x.value)),
       finalStep: getArrayString(this.state.algorithm.getFinalStep().getValue().map(x => x.value))
     });
@@ -70,6 +48,7 @@ class App extends Component {
   componentDidMount() {
     this.state.algorithm.run();
     this.setState({
+      data: this.state.algorithm.getStep(0).getValue(),
       firstStep: getArrayString(this.state.algorithm.getStep(0).getValue().map(x => x.value)),
       finalStep: getArrayString(this.state.algorithm.getFinalStep().getValue().map(x => x.value))
     });
@@ -84,7 +63,7 @@ class App extends Component {
   }
 
   render() {
-    this.printSteps();
+    // this.printSteps();
     return (
       <div className="App">
         <header className="App-header">
@@ -93,7 +72,7 @@ class App extends Component {
         <ValuesInput updateAndRun={this.updateAndRun} defaultValues={getArrayString(this.defaultValues.map(x => x.value))}/>
         <p className="App-intro">{this.state.firstStep}</p>
         <p className="App-intro">{this.state.finalStep}</p>
-        <BarChart data={data} options={options} width="600" height="250"/>
+        <SortChart chartData={this.state.data} />
       </div>
     );
   }

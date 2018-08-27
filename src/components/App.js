ed9bb2c841ba98f067256d4cb764901ffad8ced9
Luke Abel <luke.abel@gmail.com>
2018-08-26 20:30:31 -0400

    adding step input

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ValuesInput from './valuesInput';
+import StepInput from './StepInput';
 import SortChart from './SortChart';
 import {quicksort} from '../services/quicksort';
 import {createSortValue} from "../services/sortUtils";
@@ -21,16 +22,21 @@ class App extends Component {
 
   constructor(props) {
     super(props);
-    this.state = {
-      algorithm: quicksort,
-      data: []
-    }
 
     // seed default values for algorithm and input
     this.defaultValues = [4, 8, 3, 0, 2, 8, 3, 7, 9, 5];
+    this.defaultStep = 0;
     this.defaultValues = this.defaultValues.map(x => createSortValue(x));
 
+    // state
+    this.state = {
+      algorithm: quicksort,
+      data: [],
+      displayStep: this.defaultStep
+    }
+
     this.updateAndRun = this.updateAndRun.bind(this);
+    this.updateStep = this.updateStep.bind(this);
   }
 
   // used by ValuesInput to update the values & graph
@@ -38,9 +44,16 @@ class App extends Component {
     this.state.algorithm.setValues(values);
     this.state.algorithm.run();
     this.setState({
-      data: this.state.algorithm.getStep(2).getValue(),
+      data: this.state.algorithm.getStep(this.state.displayStep).getValue(),
       firstStep: getArrayString(this.state.algorithm.getStep(0).getValue().map(x => x.value)),
       finalStep: getArrayString(this.state.algorithm.getFinalStep().getValue().map(x => x.value))
+    });
+  }
+
+  updateStep(displayStep) {
+    this.setState({
+      displayStep,
+      data: this.state.algorithm.getStep(displayStep).getValue(),
     });
   }
 
@@ -64,6 +77,7 @@ class App extends Component {
           <h1 className="App-title">Algorithm Demos</h1>
         </header>
         <ValuesInput updateAndRun={this.updateAndRun} defaultValues={getArrayString(this.defaultValues.map(x => x.value))}/>
+        <StepInput updateStep={this.updateStep} defaultValue={this.defaultStep} />
         <p className="App-intro">{this.state.firstStep}</p>
         <p className="App-intro">{this.state.finalStep}</p>
         <SortChart chartData={this.state.data} />
